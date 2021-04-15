7b9272f2e21252f362bd01847f6a466a1fe83fc3
Emmanuela Joy <emmajoy81@gmail.com>
2021-04-15 06:50:12 +0300

    add business logic for the 6th test

--- src/main/java/Encoding.java
+++ src/main/java/Encoding.java
@@ -27,8 +27,41 @@ public class Encoding {
     }
 
     public String encodeText(String text, int key) {
+        String encodedText = "";
         this.input = text;
         this.shiftKey = key;
-        return text;
+        if(key>26) {
+            key = key % 26;
+        }else if(key < 0) {
+            key = (key %26) + 26;
+        }
+
+        char[] letters = text.toCharArray();
+        for(int i=0; i<letters.length; i++) {
+            char individualLetter = letters[i];
+            if(Character.isLetter(individualLetter)) {
+
+                if(Character.isUpperCase(individualLetter)) {
+                    char ch = (char) (individualLetter + key);
+                    if(ch > 'Z') {
+                        encodedText += (char) (individualLetter - (26 - key));
+                    } else {
+                        encodedText +=ch;
+                    }
+
+                } else if(Character.isLowerCase(individualLetter)) {
+                    char ch = (char) (individualLetter + key);
+                    if(ch > 'z') {
+                        encodedText += (char) (individualLetter - (26-key));
+                    } else {
+                        encodedText +=ch;
+                    }
+                }
+            } else {
+                encodedText +=individualLetter;
+            }
+        }
+
+        return encodedText;
     }
 }
