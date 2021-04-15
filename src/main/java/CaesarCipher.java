cca526c80c87e4d129313e49383ad2b5fa7f8090
Emmanuela Joy <emmajoy81@gmail.com>
2021-04-15 15:58:24 +0300

    add code that prompts the user to enter a word and key in the CaesarCipher class

--- src/main/java/CaesarCipher.java
+++ src/main/java/CaesarCipher.java
@@ -1,5 +1,19 @@
+import java.io.Console;
+
 public class CaesarCipher{
     public static void main(String[] args) {
+        Console myConsole = System.console();
+        System.out.println("Welcome to Caesar-Cipher App. Follow the steps below to encrypt and decrypt any letters or words of your choice");
+        System.out.println("What action do you want to perform? Enter one of the following options: Encrypt or Decrypt");
+        String choice = myConsole.readLine();
+        if(choice.equals("Encrypt")) {
+            System.out.println("Enter any word or letter and i will encrypt it for you");
+            String input = myConsole.readLine();
+            System.out.println("Now enter a key of any number between 1 and 25");
+            int key = Integer.parseInt(myConsole.readLine());
+            System.out.println("All set now, here is the result: ");
+            System.out.println("Your input: ");
 
+        }
     }
 }
