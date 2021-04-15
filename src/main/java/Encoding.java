5cca5a49eee76a9b29fd544ff92954c7cc9d341e
Emmanuela Joy <emmajoy81@gmail.com>
2021-04-15 06:08:20 +0300

    add business logic for the 4th test

--- src/main/java/Encoding.java
+++ src/main/java/Encoding.java
@@ -1,9 +1,14 @@
 public class Encoding {
 
+    private String input;
+    private int shiftKey;
     public String getInput(String text, int key) {
+        this.input=text;
         return text;
     }
 
-    public boolean validate(int i) {
+    public boolean validate(int key) {
+        this.shiftKey= key;
+        return(key>0 && key<26);
     }
 }
