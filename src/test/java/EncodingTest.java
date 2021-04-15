ab33e5c9fbaa38f7649c7d2b5777d4645d04ee05
Emmanuela Joy <emmajoy81@gmail.com>
2021-04-15 05:50:07 +0300

    generate first test

--- src/test/java/EncodingTest.java
+++ src/test/java/EncodingTest.java
@@ -1,5 +1,12 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class EncodingTest {
 
+    @Test
+    public void encode_instantiates_True() {
+        Encoding encoding = new Encoding();
+        assertTrue(encoding instanceof  Encoding);
+    }
 }
