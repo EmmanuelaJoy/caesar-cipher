761aa8792d5fc015561f177ade29fc95b83fb2f6
Emmanuela Joy <emmajoy81@gmail.com>
2021-04-15 13:53:00 +0300

    add business logic for the 6th test in decoding class

--- src/main/java/Decoding.java
+++ src/main/java/Decoding.java
@@ -2,6 +2,7 @@ public class Decoding {
 
     private String encodedText;
     private int inputtedKey;
+    private String decodedText = "";
 
     public Decoding(String text, int key) {
         this.encodedText = text;
@@ -31,6 +32,37 @@ public class Decoding {
     }
 
     public String decodeMultipleCharacters() {
-        return " ";
+        String text = this.getInputText();
+        int key = this.getInputtedKey();
+        String decoded = this.decodedText;
+
+        char[] letters = text.toCharArray();
+        for(int i=0; i<letters.length; i++) {
+            char individualLetter = letters[i];
+            if(Character.isLetter(individualLetter)) {
+
+                if(Character.isUpperCase(individualLetter)){
+
+                    char ch = (char)(individualLetter - key);
+                    if(ch > 'Z') {
+                        decoded += (char)(individualLetter + (26 + key));
+                    } else {
+                        decoded += ch;
+                    }
+
+                } else if(Character.isLowerCase(individualLetter)) {
+                    char ch = (char)(individualLetter - key);
+                    if(ch > 'z') {
+                        decoded += (char) (individualLetter + (26+key));
+                    } else {
+                        decoded +=ch;
+                    }
+                }
+            } else {
+                decoded += individualLetter;
+            }
+        }
+
+        return decoded;
     }
 }
