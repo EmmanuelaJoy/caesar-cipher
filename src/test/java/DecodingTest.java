f3ab8683cb0fbcd3c16e0f546e005889022e52e1
Emmanuela Joy <emmajoy81@gmail.com>
2021-04-15 07:57:20 +0300

    generate 1st test of decoding class

--- src/test/java/DecodingTest.java
+++ src/test/java/DecodingTest.java
@@ -1,5 +1,11 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class DecodingTest {
-
+    @Test
+    public void decode_Instantiates_True() {
+        Decoding decoding = new Decoding();
+        assertTrue(decoding instanceof  Decoding);
+    }
 }
